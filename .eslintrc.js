4e7de41f9c98714a8ae57a370383bfe1a7502659
Nicolas Seydoux <nseydoux@inrupt.com>
2021-08-19 18:33:39 +0200

    Align jest and eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
   extends: ["@inrupt/eslint-config-lib"],
+  overrides: [
+    {
+      files: ["**/*.test.ts"],
+      rules: {
+        // Jest is configured not to inject the globals, but eslint complains.
+        "no-shadow": "off",
+      },
+    },
+  ],
 };
