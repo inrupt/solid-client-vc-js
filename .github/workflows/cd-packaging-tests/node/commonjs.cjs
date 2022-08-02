c11c15dcac5720121eb218b90d33a892f41815dc
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2022-08-02 19:54:57 +0200

    ci: remove continuous delivery workflows
    These were poorly implemented and resulted in us publishing thousands of versions of our packages

--- .github/workflows/cd-packaging-tests/node/commonjs.cjs
+++
@@ -1,5 +0,0 @@
-const {
-  issueVerifiableCredential,
-} = require("@inrupt/solid-client-vc");
-
-console.log(typeof issueVerifiableCredential === "function");
--- .github/workflows/cd-packaging-tests/node/esmodule.mjs
+++
@@ -1,7 +0,0 @@
-// Verify that imports from the main export work:
-import { issueVerifiableCredential as mainIssueVerifiableCredential } from "@inrupt/solid-client-vc";
-// Verify that submodule imports work:
-import issueVerifiableCredential from "@inrupt/solid-client-vc/issue";
-
-console.log(typeof mainIssueVerifiableCredential === "function");
-console.log(typeof issueVerifiableCredential === "function");
