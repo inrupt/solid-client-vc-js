ace863a06a8a4fd77af226e789f71e81884ebb3a
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-05-19 14:27:10 +0200

    chore: retry e2e tests (#699)
    * chore: retry e2e tests
    
    * chore: import jest

--- e2e/node/jest.e2e.setup.ts
+++ e2e/node/jest.e2e.setup.ts
@@ -20,6 +20,12 @@
 //
 
 import { setupEnv } from "@inrupt/internal-test-env";
+import { jest } from "@jest/globals";
 
 // Fail fast on dotenv:
 setupEnv();
+
+if (process.env.CI === "true") {
+  // Tests running in the CI runners tend to be more flaky.
+  jest.retryTimes(5, { logErrorsBeforeRetry: true });
+}
