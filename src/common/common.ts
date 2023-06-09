6f73f61e84a7563719ccf16f2be65aabb7391e3e
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-06-09 11:37:50 +1000

    WIP

--- src/common/common.ts
+++ src/common/common.ts
@@ -30,17 +30,14 @@ import {
   UrlString,
   getJsonLdParser,
 } from "@inrupt/solid-client";
+import { Util } from "jsonld-streaming-serializer";
 import { fetch as uniFetch } from "@inrupt/universal-fetch";
+import { Store, DataFactory as DF } from "n3";
+import { Term, DatasetCore, Quad } from "@rdfjs/types";
+import { getVcContext, jsonLdResponseToStore } from "../parser/jsonld";
+import parse from "content-type";
 
 export type Iri = string;
-/**
- * A JSON-LD document is a JSON document including an @context entry. The other
- * fields may contain any value.
- */
-export type JsonLd = {
-  "@context": unknown;
-  [property: string]: unknown;
-};
 
 type Proof = {
   type: string;
@@ -56,7 +53,7 @@ type Proof = {
 /**
  * A Verifiable Credential JSON-LD document, as specified by the W3C VC HTTP API.
  */
-export type VerifiableCredential = JsonLd & {
+export type VerifiableCredential = {
   id: Iri;
   type: Iri[];
   issuer: Iri;
@@ -77,177 +74,13 @@ export type VerifiableCredential = JsonLd & {
   proof: Proof;
 };
 
-export type VerifiablePresentation = JsonLd & {
+export type VerifiablePresentation = {
   id?: string;
   type: string | string[];
   verifiableCredential?: VerifiableCredential[];
   holder?: string;
   proof?: Proof;
 };
-
-function isUnknownObject(x: unknown): x is {
-  [key in PropertyKey]: unknown;
-} {
-  return x !== null && typeof x === "object";
-}
-
-function hasProof(x: { [key in PropertyKey]: unknown }): x is {
-  proof: { [key in PropertyKey]: unknown };
-} {
-  return typeof x.proof !== null && typeof x.proof === "object";
-}
-
-/**
- * This function is a temporary stopgap until we implement proper JSON-LD parsing.
- * It refactors know misalignments between the JSON-LD object we receive and the
- * JSON frame we expect.
- *
- * @param vcJson A JSON-LD VC.
- * @returns an equivalent JSON-LD VC, fitted to a specific frame.
- */
-export function normalizeVc<T>(vcJson: T): T {
-  if (!isUnknownObject(vcJson) || !hasProof(vcJson)) {
-    // The received JSON doesn't have the shape we want to refactor
-    return vcJson;
-  }
-  const normalized = { ...vcJson };
-  if (
-    typeof vcJson.proof["https://w3id.org/security#proofValue"] === "string"
-  ) {
-    normalized.proof.proofValue =
-      vcJson.proof["https://w3id.org/security#proofValue"];
-    delete normalized.proof["https://w3id.org/security#proofValue"];
-  }
-  return normalized;
-}
-
-function hasCredentials(x: { [key in PropertyKey]: unknown }): x is {
-  verifiableCredential: unknown[];
-} {
-  return (
-    typeof x.verifiableCredential !== null &&
-    Array.isArray(x.verifiableCredential)
-  );
-}
-
-/**
- * Normalizes all VCs wrapped in a VP.
- *
- * @param vpJson A JSON-LD VP.
- * @returns An equivalent JSON-LD VP, with its contained VCs fitted to a specific frame.
- */
-export function normalizeVp<T>(vpJson: T): T {
-  if (!isUnknownObject(vpJson) || !hasCredentials(vpJson)) {
-    // The received JSON doesn't have the shape we want to refactor
-    return vpJson;
-  }
-  const normalizedVp = { ...vpJson };
-  normalizedVp.verifiableCredential =
-    normalizedVp.verifiableCredential.map(normalizeVc);
-
-  return normalizedVp;
-}
-
-/**
- * Verifies that a given JSON-LD payload conforms to the Verifiable Credential
- * schema we expect.
- * @param data The JSON-LD payload
- * @returns true is the payload matches our expectation.
- */
-export function isVerifiableCredential(
-  data: unknown | VerifiableCredential
-): data is VerifiableCredential {
-  let dataIsVc = true;
-  dataIsVc = typeof (data as VerifiableCredential).id === "string";
-  dataIsVc = dataIsVc && Array.isArray((data as VerifiableCredential).type);
-  dataIsVc =
-    dataIsVc && typeof (data as VerifiableCredential).issuer === "string";
-  dataIsVc =
-    dataIsVc && typeof (data as VerifiableCredential).issuanceDate === "string";
-  dataIsVc =
-    dataIsVc &&
-    !Number.isNaN(Date.parse((data as VerifiableCredential).issuanceDate));
-  dataIsVc =
-    dataIsVc &&
-    typeof (data as VerifiableCredential).credentialSubject === "object";
-  dataIsVc =
-    dataIsVc &&
-    typeof (data as VerifiableCredential).credentialSubject.id === "string";
-  dataIsVc =
-    dataIsVc && typeof (data as VerifiableCredential).proof === "object";
-  dataIsVc =
-    dataIsVc &&
-    typeof (data as VerifiableCredential).proof.created === "string";
-  dataIsVc =
-    dataIsVc &&
-    !Number.isNaN(Date.parse((data as VerifiableCredential).proof.created));
-  dataIsVc =
-    dataIsVc &&
-    typeof (data as VerifiableCredential).proof.proofPurpose === "string";
-  dataIsVc =
-    dataIsVc &&
-    typeof (data as VerifiableCredential).proof.proofValue === "string";
-  dataIsVc =
-    dataIsVc && typeof (data as VerifiableCredential).proof.type === "string";
-  dataIsVc =
-    dataIsVc &&
-    typeof (data as VerifiableCredential).proof.verificationMethod === "string";
-  return dataIsVc;
-}
-
-function isUrl(url: string): boolean {
-  try {
-    // If url is not URL-shaped, this will throw.
-    // eslint-disable-next-line no-new
-    new URL(url);
-    return true;
-  } catch (_e) {
-    return false;
-  }
-}
-
-export function isVerifiablePresentation(
-  vp: unknown | VerifiablePresentation
-): vp is VerifiablePresentation {
-  let inputIsVp = true;
-  inputIsVp =
-    inputIsVp &&
-    (Array.isArray((vp as VerifiablePresentation).type) ||
-      typeof (vp as VerifiablePresentation).type === "string");
-  if ((vp as VerifiablePresentation).verifiableCredential !== undefined) {
-    inputIsVp =
-      inputIsVp &&
-      Array.isArray((vp as VerifiablePresentation).verifiableCredential);
-    inputIsVp =
-      inputIsVp &&
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      (vp as VerifiablePresentation).verifiableCredential!.every(
-        isVerifiableCredential
-      );
-  }
-  if ((vp as VerifiablePresentation).holder !== undefined) {
-    inputIsVp =
-      inputIsVp && typeof (vp as VerifiablePresentation).holder === "string";
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    inputIsVp = inputIsVp && isUrl((vp as VerifiablePresentation).holder!);
-  }
-  // TODO: No type checking is currently implemented for the proof.
-  return inputIsVp;
-}
-
-export function concatenateContexts(...contexts: unknown[]): unknown {
-  const result: Set<unknown> = new Set();
-  contexts.forEach((additionalContext) => {
-    // Case when the context is an array of IRIs and/or inline contexts
-    if (Array.isArray(additionalContext)) {
-      additionalContext.forEach((context) => result.add(context));
-    } else if (additionalContext !== null && additionalContext !== undefined) {
-      // Case when the context is a single remote URI or a single inline context
-      result.add(additionalContext);
-    }
-  });
-  return Array.from(result.values());
-}
 
 /**
  * This context contains the required elements to build a valid VC issuance request.
@@ -397,6 +230,60 @@ export async function getVerifiableCredentialApiConfiguration(
   };
 }
 
+const VC = "https://www.w3.org/2018/credentials#";
+const RDF = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+const XSD = "http://www.w3.org/2001/XMLSchema#";
+const SEC = "https://w3id.org/security#";
+const CRED = "https://www.w3.org/2018/credentials#";
+
+/**
+ * @param response Takes a response from a VC service and checks that it has the correct status and content type
+ * @param vcUrl The URL of the VC
+ * @returns The input response
+ */
+function validateVcResponse(response: globalThis.Response, vcUrl: string): globalThis.Response {
+  if (!response.ok) {
+    throw new Error(
+      `Fetching the Verifiable Credential [${vcUrl}] failed: ${response.status} ${response.statusText}`
+    );
+  }
+
+  const contentType = response.headers.get("Content-Type");
+
+  if (!contentType) {
+    throw new Error(`Fetching the Verifiable Credential [${vcUrl}] failed: Response does not have a Content-Type header; expected application/ld+json`)
+  }
+
+  const parsedContentType = parse.parse(contentType);
+  const [mediaType, subtypesString] = parsedContentType.type.split("/");
+  const subtypes = subtypesString.split('+');
+
+  if (mediaType !== "application" || !subtypes.includes('ld') || !subtypes.includes('json')) {
+    throw new Error(`Fetching the Verifiable Credential [${vcUrl}] failed: Response has an unsupported Content-Type header; expected application/ld+json`)
+  }
+
+  return response;
+}
+
+async function fetchVcStore(
+  vcUrl: UrlString,
+  options?: Partial<{
+    fetch: typeof fetch;
+  }>
+): Promise<Store> {
+  const authFetch = options?.fetch ?? uniFetch;
+  // FIXME: See if we need content negotiation here
+  const response = validateVcResponse(await authFetch(vcUrl), vcUrl);
+
+  try {
+    return await jsonLdResponseToStore(response, { fetch: authFetch });
+  } catch (e) {
+    throw new Error(
+      `Parsing the Verifiable Credential [${vcUrl}] as JSON-LD failed: ${e}`
+    );
+  }
+}
+
 /**
  * Dereference a VC URL, and verify that the resulting content is valid.
  *
@@ -411,29 +298,217 @@ export async function getVerifiableCredential(
   options?: Partial<{
     fetch: typeof fetch;
   }>
-): Promise<VerifiableCredential> {
-  const authFetch = options?.fetch ?? uniFetch;
-  return authFetch(vcUrl as string)
-    .then(async (response) => {
-      if (!response.ok) {
-        throw new Error(
-          `Fetching the Verifiable Credential [${vcUrl}] failed: ${response.status} ${response.statusText}`
-        );
+): Promise<VerifiableCredential & DatasetCore> {
+  const vcStore = await fetchVcStore(vcUrl, options);
+  const context = await getVcContext();
+
+  const vcs = vcStore.getSubjects(
+    `${RDF}type`,
+    `${VC}VerifiableCredential`,
+    DF.defaultGraph()
+  );
+  if (vcs.length !== 1) {
+    throw new Error(
+      `Expected exactly one Verifiable Credential in [${vcUrl}], received: ${vcs.length}`
+    );
+  }
+
+  const [vc] = vcs;
+  if (vc.termType !== "NamedNode") {
+    throw new Error(
+      `Expected the Verifiable Credential in [${vcUrl}] to be a Named Node, received: ${vc.termType}`
+    );
+  }
+
+  const type: string[] = [];
+  for (const t of vcStore.getObjects(vc, `${RDF}type`, DF.defaultGraph())) {
+    if (t.termType !== "NamedNode") {
+      throw new Error(
+        `Expected all VC types to be Named Nodes but received [${t.value}] of termType [${t.termType}]`
+      );
+    }
+
+    const compact = context.compactIri(t.value, true);
+
+    if (/^[a-z]+$/i.test(compact))
+      type.push(compact);
+  }
+
+  function getSingleObject(
+    fullProperty: string,
+    subject?: Term,
+    graph: Term = DF.defaultGraph()
+  ): Term {
+    const objects = vcStore.getObjects(subject ?? vc, fullProperty, graph);
+
+    if (objects.length !== 1) {
+      throw new Error(
+        `Expected exactly one [${fullProperty}] for the Verifiable Credential ${vc.value}, received: ${objects.length}`
+      );
+    }
+
+    return objects[0];
+  }
+
+  function getSingleObjectOfTermType(
+    fullProperty: string,
+    subject?: Term,
+    graph?: Term,
+    termType = "NamedNode"
+  ) {
+    const object = getSingleObject(fullProperty, subject, graph);
+
+    if (object.termType !== termType) {
+      throw new Error(
+        `Expected property [${fullProperty}] of the Verifiable Credential [${vc.value}] to be a ${termType}, received: ${object.termType}`
+      );
+    }
+
+    // TODO: Make sure that Literals with URIs are correclty handled here
+    return object.value;
+  }
+
+  function getSingleDateTime(
+    fullProperty: string,
+    subject?: Term,
+    graph?: Term
+  ) {
+    const object = getSingleObject(fullProperty, subject, graph);
+
+    if (object.termType !== "Literal") {
+      throw new Error(
+        `Expected issuanceDate to be a Literal, received: ${object.termType}`
+      );
+    }
+    if (!object.datatype.equals(DF.namedNode(`${XSD}dateTime`))) {
+      throw new Error(
+        `Expected issuanceDate to have dataType [${XSD}dateTime], received: [${object.datatype.value}]`
+      );
+    }
+
+    return object.value;
+  }
+
+  // The proof lives within a named graph
+  const proofGraph = getSingleObject(`${SEC}proof`);
+  const proofs = vcStore.getSubjects(null, null, proofGraph);
+
+  if (proofs.length !== 1) {
+    throw new Error(
+      `Expected exactly one proof to live in the proofs graph, received ${proofs.length}`
+    );
+  }
+
+  const [proof] = proofs;
+  function getProperties(subject: Term, writtenTerms: string[] = []) {
+    const object: Record<string, unknown> = {};
+
+    for (const predicate of vcStore.getPredicates(
+      subject,
+      null,
+      DF.defaultGraph()
+    )) {
+      if (predicate.termType !== "NamedNode") {
+        throw new Error("Predicate must be a named node")
       }
-      try {
-        return normalizeVc(await response.json());
-      } catch (e) {
-        throw new Error(
-          `Parsing the Verifiable Credential [${vcUrl}] as JSON failed: ${e}`
-        );
+
+      const compact = context.compactIri(predicate.value, true);
+      if (/^[a-z]+$/i.test(compact)) {
+        const objects: any[] = vcStore.getObjects(
+          subject,
+          predicate,
+          DF.defaultGraph()
+        ).map(object => writeObject(object, writtenTerms))
+          .filter(object => typeof object !== "object" || Object.keys(object).length > 1)
+
+        if (objects.length === 1) {
+          object[compact] = objects[0]
+        } else if (objects.length > 1) {
+          object[compact] = objects
+        }
       }
-    })
-    .then((vc) => {
-      if (!isVerifiableCredential(vc)) {
-        throw new Error(
-          `The value received from [${vcUrl}] is not a Verifiable Credential`
-        );
-      }
-      return vc;
-    });
+    }
+
+    return object;
+  }
+
+  function writeObject(object: Term, writtenTerms: string[] = []) {
+    switch (object.termType) {
+      case "BlankNode":
+        return writtenTerms.includes(object.value)
+          ? {}
+          : getProperties(object, [...writtenTerms, object.value]);
+      case "NamedNode":
+        // TODO: See if we actually want to do compacting here
+        // given how ConsentStatusExplicitlyGiven as the full
+        // URI in e2e tests, and this may make it look like a
+        // literal
+        return context.compactIri(object.value, true);
+      case "Literal":
+        return Util.termToValue(object, context);
+      default:
+        throw new Error("Unexpected term type")
+    }
+  }
+
+  const credentialSubject = getSingleObjectOfTermType(`${CRED}credentialSubject`);
+  return {
+    id: vc.value,
+    // It is possible to have multiple claims in a credential subject
+    // we do not support this
+    // https://www.w3.org/TR/vc-data-model/#example-specifying-multiple-subjects-in-a-verifiable-credential
+    credentialSubject: {
+      ...getProperties(DF.namedNode(credentialSubject)),
+      id: credentialSubject
+    },
+    issuer: getSingleObjectOfTermType(`${CRED}issuer`),
+    issuanceDate: getSingleDateTime(`${CRED}issuanceDate`),
+    type,
+    proof: {
+      created: getSingleDateTime(
+        "http://purl.org/dc/terms/created",
+        proof,
+        proofGraph
+      ),
+      proofPurpose: getSingleObjectOfTermType(
+        `${SEC}proofPurpose`,
+        proof,
+        proofGraph
+      ),
+      type: getSingleObjectOfTermType(`${RDF}type`, proof, proofGraph),
+      verificationMethod: getSingleObjectOfTermType(
+        `${SEC}verificationMethod`,
+        proof,
+        proofGraph
+      ),
+      proofValue: getSingleObjectOfTermType(
+        `${SEC}proofValue`,
+        proof,
+        proofGraph,
+        "Literal"
+      ),
+    },
+
+    // Make this a DatasetCore without polluting the object with
+    // all of the properties present in the N3.Store
+    [Symbol.iterator]() {
+      return vcStore[Symbol.iterator]();
+    },
+    has(quad) {
+      return vcStore.has(quad);
+    },
+    match(subject, predicate, object, graph) {
+      // @ts-ignore
+      return vcStore.match(subject, predicate, object, graph);
+    },
+    add(quad) {
+      throw new Error("Cannot mutate this dataset");
+    },
+    delete(quad) {
+      throw new Error("Cannot mutate this dataset");
+    },
+    get size() {
+      return vcStore.size;
+    }
+  };
 }
