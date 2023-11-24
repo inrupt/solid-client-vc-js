a377e6fb94741d25a9fe5ed31225c5b658e7d051
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-11-24 10:42:24 +0000

    chore: add links for cached contexts

--- src/parser/contexts/inrupt-vc.ts
+++ src/parser/contexts/inrupt-vc.ts
@@ -18,6 +18,10 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 // SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
+
+/**
+ * @see https://vc.inrupt.com/credentials/v1
+ */
 export default {
   "@context": {
     "@version": 1.1,
--- src/parser/contexts/status-list.ts
+++ src/parser/contexts/status-list.ts
@@ -18,6 +18,10 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 // SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
+
+/**
+ * @see https://w3id.org/vc/status-list/2021/v1
+ */
 export default {
   "@context": {
     "@protected": true,
