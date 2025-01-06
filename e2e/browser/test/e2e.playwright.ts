d042262bb36f4ae7df810da053477dee608d70f3
Nicolas Ayral Seydoux <nseydoux@inrupt.com>
2025-01-06 10:50:14 +0100

    Fix order of expectations

--- e2e/browser/test/e2e.playwright.ts
+++ e2e/browser/test/e2e.playwright.ts
@@ -72,10 +72,10 @@ test("Try issuing an invalid credential, get an error", async ({
 
   // Try to issue VC with invalid credentials
   await Promise.all([
-    page.click("button[data-testid=issue-invalid-vc]"),
-    page.click("button[data-nextjs-errors-dialog-left-right-close-button]"),
     page.waitForRequest((request) => request.method() === "POST"),
     page.waitForResponse((response) => response.status() === 400),
+    page.click("button[data-testid=issue-invalid-vc]"),
+    page.click("button[data-nextjs-errors-dialog-left-right-close-button]"),
   ]);
   // Validate we get nothing in response
   await expect(
